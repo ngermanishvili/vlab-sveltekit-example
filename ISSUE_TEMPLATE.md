3b1f6fc6b5c8d23fb03ced119662d4277f6f59ce
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2024-02-22 03:25:40 +0400

    VLAB Svelte example

---
+++ ISSUE_TEMPLATE.md
@@ -0,0 +1,12 @@
+<!--
+ IMPORTANT: Please use the following link to create a new issue:
+
+            href="https://visionarylabs.io/"
+
+**If your issue was not created using the app above, it will be closed immediately.**
+-->
+
+<!--
+            href="https://visionarylabs.io/"
+            href="https://visionarylabs.io/"
+-->
---
+++ src/components/Footers/FooterSmall.svelte
@@ -0,0 +1,67 @@
+<script>
+  // make dynamic date to be added to footer
+  let date = new Date().getFullYear();
+
+  export let absolute = false;
+</script>
+
+<footer
+  class="pb-6 {absolute
+    ? 'absolute w-full bottom-0 bg-blueGray-800'
+    : 'relative'}"
+>
+  <div class="container mx-auto px-4">
+    <hr class="mb-6 border-b-1 border-blueGray-600" />
+    <div class="flex flex-wrap items-center md:justify-between justify-center">
+      <div class="w-full md:w-4/12 px-4">
+        <div
+          class="text-sm text-blueGray-500 font-semibold py-1 text-center md:text-left"
+        >
+          Copyright © {date}
+          <a
+            href="https://visionarylabs.io/"
+            class="text-white hover:text-blueGray-300 text-sm font-semibold py-1"
+          >
+            Visionary Lab
+          </a>
+        </div>
+      </div>
+      <div class="w-full md:w-8/12 px-4">
+        <ul class="flex flex-wrap list-none md:justify-end justify-center">
+          <li>
+            <a
+              href="https://visionarylabs.io/"
+              class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
+            >
+              Visionary Lab
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://visionarylabs.io/"
+              class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
+            >
+              About Us
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://visionarylabs.io/"
+              class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
+            >
+              Blog
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://visionarylabs.io/"
+              class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
+            >
+              MIT License
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
